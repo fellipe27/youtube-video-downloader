57473e209b77ccbcb89a0692195dce7fffb2c8aa
fellipe27 <felipe27pffl@gmail.com>
2024-01-25 09:27:47 -0300

    first commit

--- main.py
+++ main.py
@@ -17,31 +17,31 @@ class Window:
         ]
         self.title = 'Youtube Video Downloader'
         self.window = sg.Window(self.title, self.layout)
-        self.downloadComplete = False
-        self.haveError = False
-        self.videoTitle = ''
+        self.download_complete = False
+        self.have_error = False
+        self.video_title = ''
 
-    def download(self, url, path, onlyAudio):
+    def download(self, url, path, only_audio):
         try:
             requests.get('https://github.com/fellipe27')
         except requests.ConnectionError:
-            self.haveError = True
+            self.have_error = True
         else:
             try:
                 yt = YouTube(url)
                 resolution = yt.streams.get_highest_resolution()
 
-                yt.streams.filter(only_audio=onlyAudio, resolution=resolution).first().download(path)
-                self.videoTitle = yt.title
+                yt.streams.filter(only_audio=only_audio, resolution=resolution).first().download(path)
+                self.video_title = yt.title
             except RegexMatchError:
-                self.haveError = True
+                self.have_error = True
             else:
-                self.haveError = False
+                self.have_error = False
             finally:
-                self.downloadComplete = True
+                self.download_complete = True
 
-    def downloadThread(self, url, path, onlyAudio):
-        threading.Thread(target=self.download, args=(url, path, onlyAudio), daemon=True).start()
+    def download_thread(self, url, path, only_audio):
+        threading.Thread(target=self.download, args=(url, path, only_audio), daemon=True).start()
 
     def run(self):
         while True:
@@ -50,33 +50,33 @@ class Window:
             if event == sg.WIN_CLOSED or event == 'Exit':
                 break
             elif event == 'Download':
-                urlValue = values['url']
-                pathValue = values['path']
-                onlyAudio = False if values['format_0'] else True
+                url_value = values['url']
+                path_value = values['path']
+                only_audio = False if values['format_0'] else True
 
-                if urlValue and pathValue:
+                if url_value and path_value:
                     self.window['status'].update('Download in progress...')
                     self.window['Download'].update(disabled=True)
 
-                    self.downloadThread(urlValue, pathValue, onlyAudio)
+                    self.download_thread(url_value, path_value, only_audio)
 
-                    while not self.downloadComplete:
+                    while not self.download_complete:
                         event, _ = self.window.read(timeout=100)
                         if event == sg.WIN_CLOSED or event == 'Exit':
                             break
 
-                    if self.downloadComplete:
-                        if self.haveError:
+                    if self.download_complete:
+                        if self.have_error:
                             sg.popup('Something is wrong! :(', title='No connection!')
                         else:
-                            sg.popup(f'{self.videoTitle} has been downloaded', title='Download successful!')
+                            sg.popup(f'{self.video_title} has been downloaded', title='Download successful!')
                             self.window['url'].update('')
                             self.window['path'].update('')
                             self.window['format_0'].update(value=True)
                             self.window['Download'].update(disabled=False)
 
                         self.window['status'].update('Download your favorite Youtube video <3')
-                        self.downloadComplete = False
+                        self.download_complete = False
 
         self.window.close()
 
